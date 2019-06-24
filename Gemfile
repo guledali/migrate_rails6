fe20eb7727baee8d9c78e97051aaac9c852fd72c
“guledali” <guled.ali01@gmail.com>
2019-06-24 20:53:36 +0200

    Finished module-3

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootsnap', '>= 1.4.1', require: false
 
 # webbit custom gems
 gem 'devise', '~> 4.6', '>= 4.6.2'
+gem 'carrierwave', '~> 1.3', '>= 1.3.1'
+gem 'mini_magick', '~> 4.9', '>= 4.9.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_103753) do
+ActiveRecord::Schema.define(version: 2019_06_24_181329) do
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "submission_image"
+    t.string "submission_video"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_submissions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_06_24_103753) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "submissions", "users"
 end

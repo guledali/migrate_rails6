8886f7f745cc642c1b37b0448d21b3c8a192bc0c
“guledali” <guled.ali01@gmail.com>
2019-07-14 16:32:57 +0200

    Finished module-10

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,11 @@
 class Community < ApplicationRecord
   belongs_to :user
   has_many :submissions
+
+  before_save :format_name
+
+  def format_name
+    self.name.titleize
+    self.name.gsub!(' ','')
+  end
 end
